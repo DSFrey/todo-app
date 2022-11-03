e79a13c4095743010d1b096de42db316c753b3b0
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-11-03 02:52:50 +0000

    Create Auth context and wrapper

--- src/index.jsx
+++ src/index.jsx
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { MantineProvider } from '@mantine/core';
 import { App } from './app';
 import { SettingsProvider } from './Context/settings';
+import { AuthProvider } from './Context/auth';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <SettingsProvider>
-        <App />
-      </SettingsProvider>
+      <AuthProvider>
+        <SettingsProvider>
+          <App />
+        </SettingsProvider>
+      </AuthProvider>
     </MantineProvider>
   </React.StrictMode>
 );
