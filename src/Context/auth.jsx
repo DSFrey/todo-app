1503d14c6bb0a90d73b6a1f6168783d42bd36111
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-11-06 02:14:16 +0000

    Log in to api server instead of local

--- src/Context/auth.jsx
+++ src/Context/auth.jsx
@@ -2,27 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { useCookies } from 'react-cookie'
 import jwt_decode from 'jwt-decode';
-
-const testUsers = {
-  admin: {
-    username: 'admin',
-    password: 'ADMIN',
-    email: 'admin@fakeuser.com',
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZTBlZDFiMzNjZTQ5MDAxODlmMzhiNyIsImNhcGFiaWxpdGllcyI6WyJjcmVhdGUiLCJ1cGRhdGUiLCJyZWFkIiwiZGVsZXRlIl0sInR5cGUiOiJ1c2VyIiwiaWF0IjoxNjU4OTA3OTMxLCJleHAiOjE2NTg5MTE1MzF9.bqe-52if5K50rGn30P4fheuAa2qWuxse9tWiuH4cnUM',
-  },
-  editor: {
-    username: 'editor',
-    password: 'EDITOR',
-    email: 'editor@fakeuser.com',
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZTBlZjk5MzNjZTQ5MDAxODlmMzhiYSIsImNhcGFiaWxpdGllcyI6WyJjcmVhdGUiLCJ1cGRhdGUiLCJyZWFkIl0sInR5cGUiOiJ1c2VyIiwiaWF0IjoxNjU4OTA4NTY5LCJleHAiOjE2NTg5MTIxNjl9.073ppQCHbplYN9befn8JElcP4zgFX6TEe_ARUQZc0KU',
-  },
-  user: {
-    username: 'user',
-    password: 'USER',
-    email: 'user@fakeuser.com',
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZTBmMGZjMzNjZTQ5MDAxODlmMzhjMCIsImNhcGFiaWxpdGllcyI6WyJyZWFkIl0sInR5cGUiOiJ1c2VyIiwiaWF0IjoxNjU4OTA4OTI0LCJleHAiOjE2NTg5MTI1MjR9.t7c7k2LbaTxsdfYjx_WC3QiP4MycU8sZryVyXQqJQH',
-  }
-}
+import { useAxios } from "../hooks/api";
 
 export const AuthContext = React.createContext();
 
@@ -53,15 +33,22 @@ export const AuthProvider = ({ children }) => {
   }
 
   const login = (username, password) => {
-    let authCredentials = testUsers[username];
-
-    if (authCredentials?.password === password) {
-      try {
-        _validateToken(authCredentials.token);
-      } catch (e) {
-        setError(e);
-        console.error(e);
-      }
+    try {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      const { response, error } = useAxios({
+        baseURL: 'https://api-js401.herokuapp.com/',
+        url: '/signin',
+        method: 'post',
+        auth: {
+          username,
+          password
+        }
+      })
+      setError(error)
+      _validateToken(response.token);
+    } catch (e) {
+      setError(e);
+      console.error(e);
     }
   }
   const logout = () => {
